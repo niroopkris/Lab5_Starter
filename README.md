75bdad680893fe4a7caf43caef7a704dee27d893
Niroop Krishnakumar <niroopkris@gmail.com>
2025-05-05 18:13:09 -0700

    testing branch: should be wrong; also added other custom tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Lab 5 - Starter
 Lab Partners: Niroop Krishnakumar, Sarah Chun
+
+## Explore Questions
+1) For simply testing if this "message" feature works or not, then no I would not make that a unit test, as that's somewhat of a vague and unnecessary test. I feel like for broader functions like this, you want to make unit tests for specific aspects of it, such as if it correctly meets certain conditions (ex: within a given length) or if it is invalid in any way.
+2) Yes, because unlike the previous example, this is a very specific and self-contained feature that can be tested for easily by taking a message input and seeing if it meets the "less than 80 characters" condition. 
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,95 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+// isPhoneNumber Tests (first 2 are TRUE, last 2 are FALSE)
+test('check correct phone number with parentheses', () => {
+  expect(isPhoneNumber("(123) 456-7899")).toBe(true);
+});
+
+test('check correct number with dashes', () => {
+  expect(isPhoneNumber("123-456-7899")).toBe(true);
+});
+
+test('check incorrect number too few numbers', () => {
+  expect(isPhoneNumber("56-7899")).toBe(false);
+});
+
+test('check incorrect number with invalid format', () => {
+  expect(isPhoneNumber("123 456 7899")).toBe(false);
+});
+
+
+
+// isEmail
+test('check correct standard email', () => {
+  expect(isEmail("batman@gmail.com")).toBe(true);
+});
+
+test('check correct email with underscore in username', () => {
+  expect(isEmail("bat_man@gmail.com")).toBe(true);
+});
+
+test('check number with a invalid character in username', () => {
+  expect(isEmail("bat.man@gmail.com")).toBe(false);
+});
+
+test('check number with incomplete domain', () => {
+  expect(isEmail("batman@gmail")).toBe(false);
+});
+
+
+
+// isStrongPassword
+test('check a basic strong password', () => {
+  expect(isStrongPassword("password789")).toBe(true);
+});
+
+test('check password with an underscore ', () => {
+  expect(isStrongPassword("pass_word789")).toBe(true);
+});
+
+test('check invalid password starting with a number', () => {
+  expect(isStrongPassword("789password")).toBe(false);
+});
+
+test('check password with invalid special characters', () => {
+  expect(isStrongPassword("pass!word.789")).toBe(false);
+});
+
+
+
+// isDate
+test('check date in MM/DD/YYYY format', () => {
+  expect(isDate("10/10/2000")).toBe(true);
+});
+
+test('check date in M/D/YYYY format', () => {
+  expect(isDate("1/1/2000")).toBe(true);
+});
+
+test('check invalid date with a year that is not 4 digits', () => {
+  expect(isDate("1/1/20")).toBe(false);
+});
+
+test('check invalid date with improper / format', () => {
+  expect(isDate("10-10-2000")).toBe(false);
+});
+
+
+// isHexColor
+test('check valid 6 character hex code without hash', () => {
+  expect(isHexColor("FFFFFF")).toBe(true);
+});
+
+test('check valid 3 character hex code with hash', () => {
+  expect(isHexColor("000")).toBe(true);
+});
+
+test('check hex code with invalid characters', () => {
+  expect(isHexColor("#FFFFFG")).toBe(false);
+});
+
+test('check hex code with invalid length', () => {
+  expect(isHexColor("FF")).toBe(false);
+});
